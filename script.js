75938fac093c53c233c0031db484aa4c489862da
Husnain Syed <69011172+DaimSyed@users.noreply.github.com>
2021-03-27 22:07:56 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,167 @@
+const form = document.getElementById("form")
+const input = document.getElementById("input")
+const todosUL = document.getElementById("todos")
+
+const localList = JSON.parse(localStorage.getItem("list"))
+
+localList.forEach(text =>{
+    makingList(text)
+})
+
+
+form.addEventListener("submit", (e)=>{
+    e.preventDefault()
+
+    makingList()
+
+})
+
+
+function makingList(localObject){
+    let textOfInput = input.value
+
+    if(localObject){
+        textOfInput = localObject.text
+    }
+
+
+    if(textOfInput){
+        const makeLi = document.createElement("li")
+        makeLi.innerText  = textOfInput
+          
+        if(localObject && localObject.completed){
+            makeLi.classList.add("comlete")
+        } 
+
+        makeLi.addEventListener("click",()=>{
+            makeLi.classList.toggle("comlete")
+            storingLiText()
+        })
+
+        makeLi.addEventListener("contextmenu",(e)=>{
+            e.preventDefault()
+            makeLi.remove()
+            storingLiText()
+        })
+
+        todosUL.appendChild(makeLi)
+
+        input.value = ''
+        
+        storingLiText()
+    }
+}
+
+function storingLiText(){
+    const liAll = document.querySelectorAll("li")
+    const arrayOftext = []
+
+    liAll.forEach(li=>{
+        arrayOftext.push({
+            text: li.innerText,
+            completed: li.classList.contains("comlete")
+        })
+    })
+
+    localStorage.setItem('list', JSON.stringify(arrayOftext))
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const todos = JSON.parse(localStorage.getItem('todos'))
+
+// if(todos){
+//     todos.forEach(todo => {
+//         addTodo(todo)
+        
+//     });
+// }
+
+// form.addEventListener("submit", (e)=>{
+//     e.preventDefault()
+
+//     addTodo()
+// })
+
+// function addTodo(todo){
+//     let todoText = input.value
+
+//     if(todo){
+//         todoText = todo.text
+//     }
+
+//     if(todoText){
+//         const todoEl = document.createElement("li")
+//         if( todo && todo.completed){
+//             todoEl.classList.add("completed")
+//         }
+
+//         todoEl.innerText = todoText
+        
+//         todoEl.addEventListener("click", ()=>{
+//             todoEl.classList.toggle("comlete")
+//             updatels()
+//         })
+
+//         todoEl.addEventListener("contextmenu", (e)=>{
+//             e.preventDefault()
+//             todoEl.remove()
+//             updatels()
+//         })
+
+//         todosUL.appendChild(todoEl)
+
+//         input.value = ''
+
+//         updatels()
+//     }
+
+// }
+
+// function updatels(){
+//     const todoEl = document.querySelectorAll("li")
+
+//     const todos = []
+
+//     todoEl.forEach(todo =>{
+//         todos.push({
+//             text: todo.innerText,
+//             completed:todo.classList.contains("comlete")
+//         })
+//     })
+
+//     localStorage.setItem("todos", JSON.stringify(todos))
+// }
